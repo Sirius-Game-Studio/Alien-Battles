25e87cfdbf2cdded423bfe44187326b8566ac086
Roopkotha Studio <roopkotha7d@gmail.com>
2020-01-20 09:56:05 +0600

    Created script

---
+++ Assets/Scripts/Core/ScoreCount.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCount : MonoBehaviour
+{
+    [SerializeField] private Text count = null;
+
+    void Update()
+    {
+        if (GameController.instance.gamemode == GameController.Gamemodes.Campaign)
+        {
+            gameObject.SetActive(false);
+        } else if (GameController.instance.gamemode == GameController.Gamemodes.Endless)
+        {
+            gameObject.SetActive(true);
+            count.text = GameController.instance.score.ToString();
+        }
+    }
+}
